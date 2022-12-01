595d205376e0a8514d4f8f4e3c4b0ee80498899c
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-12-01 14:52:04 -0500

    update

--- js/keys.js
+++
@@ -1,3 +0,0 @@
-const mapboxToken = 'pk.eyJ1IjoidmFuZHlrZW5uaWNob2xhcyIsImEiOiJjbGExOW5kbDIwMXQ1NDFtcmJycjBxMG8yIn0.q7VgExoDOZcS3HMI1ejfbQ';
-const weatherMapKey = `8d89ba53344ea20bc4cc8bd75264ee45`
-const githubToken = `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`;
